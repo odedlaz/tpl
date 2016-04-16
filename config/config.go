8ac94d0c0c0668e265b1ea6bb9760a0273f54a23
Oded Lazar <odedlaz@gmail.com>
2016-04-16 19:24:21 +0300

    made config error handling better

--- config/config.go
+++ config/config.go
@@ -8,6 +8,8 @@ import (
 	"github.com/ghodss/yaml"
 )
 
+const DEFAULT_FILENAME = "tpl.yml"
+
 // Settings tpl settings
 type Settings struct {
 	KV struct {
--- tpl.go
+++ tpl.go
@@ -17,44 +17,42 @@ import (
 // VERSION current version of the app
 const VERSION = "0.1"
 
+// default config path
+
 func printVersion(ctx *kingpin.ParseContext) error {
 	fmt.Printf("tpl %s\n", VERSION)
 	fmt.Printf("filters: %s\n", strings.Join(tpl.Filters, ", "))
-
 	os.Exit(0)
 	return nil
+}
+
+func handleUnhandledError() {
+	if err := recover(); err != nil {
+		app.Fatalf(fmt.Sprint(err))
+	}
 }
 
 var (
 	app              = kingpin.New("tpl", "A command-line un-templating application.")
 	templateFilename = app.Arg("filename", "path to a file containing a valid djanto template (DTL)").ExistingFile()
-	configFilename   = app.Flag("config", "path to config file").Default("tpl.yaml").String()
+	configFilename   = app.Flag("config", "path to config file").Default(config.DEFAULT_FILENAME).String()
 	showVersion      = app.Flag("version", "show version and quit").Action(printVersion).Bool()
 )
 
 func main() {
-	// fail gracefully
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-	}()
+	defer handleUnhandledError()
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	if *templateFilename == "" && !tplos.StdInAvailable() {
 		app.Fatalf("you have to pipe text or pass a template filename as argument, try --help")
-		os.Exit(1)
 	}
-
 	// if settings can't be loaded, the default is used
-	if settings, err := config.Load(*configFilename); err != nil {
-		fmt.Fprintf(os.Stderr,
-			"couldn't load configuration from '%s', ignoring configuration.\n",
-			*configFilename)
-	} else {
+	// if the file doesn't exit -> ignore
+	if settings, err := config.Load(*configFilename); err == nil {
 		tpl.RegisterSettings(settings)
+	} else if !os.IsNotExist(err) || *configFilename != config.DEFAULT_FILENAME {
+		app.Fatalf("loading config from: '%s': %v, try --help", *configFilename, err.Error())
 	}
 
 	templateText := tpl.Must(tpl.Read(*templateFilename))
