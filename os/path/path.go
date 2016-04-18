3947c16ce9b8d174af14b067ee59c855e225dcf1
Oded Lazar <odedlaz@gmail.com>
2016-04-18 16:23:22 +0300

    added sed-like in place flag

--- os/path/path.go
+++ os/path/path.go
@@ -10,3 +10,13 @@ func IsDir(path string) (bool, error) {
 	}
 	return fileInfo.IsDir(), nil
 }
+
+// FileMode returns the file mode bits of a file
+func FileMode(path string) (os.FileMode, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+	// file mode bits
+	return fileInfo.Mode(), nil
+}
--- tpl.go
+++ tpl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -11,6 +12,7 @@ import (
 	// load all the filters
 	_ "github.com/odedlaz/tpl/filters"
 	tplos "github.com/odedlaz/tpl/os"
+	tplpath "github.com/odedlaz/tpl/os/path"
 	tpl "github.com/odedlaz/tpl/template"
 )
 
@@ -32,8 +34,26 @@ func handleUnhandledError() {
 	}
 }
 
+func editInPlace(text, templateText string) {
+	mode, err := tplpath.FileMode(*templateFilename)
+	if err != nil {
+		app.Fatalf("edit file (%s) in place: %v, try --help", *configFilename, err.Error())
+	}
+
+	filesToWrite := map[string]string{
+		fmt.Sprintf("%s.bak", *templateFilename): templateText,
+		*templateFilename:                        text}
+
+	for filename, txt := range filesToWrite {
+		ioutil.WriteFile(filename,
+			[]byte(txt),
+			mode)
+	}
+}
+
 var (
 	app              = kingpin.New("tpl", "A command-line un-templating application.")
+	edit             = app.Flag("in-place", "edit files in place (if --filename was supplied)").Short('i').Bool()
 	templateFilename = app.Arg("filename", "path to a file containing a valid djanto template (DTL)").ExistingFile()
 	configFilename   = app.Flag("config", "path to config file").Default(config.DEFAULT_FILENAME).String()
 	showVersion      = app.Flag("version", "show version and quit").Action(printVersion).Bool()
@@ -47,6 +67,11 @@ func main() {
 	if *templateFilename == "" && !tplos.StdInAvailable() {
 		app.Fatalf("you have to pipe text or pass a template filename as argument, try --help")
 	}
+
+	if *edit && *templateFilename == "" {
+		app.Fatalf("you can't edit a file if you're piping the template, try --help")
+	}
+
 	// if settings can't be loaded, the default is used
 	// if the file doesn't exit -> ignore
 	if settings, err := config.Load(*configFilename); err == nil {
@@ -59,5 +84,9 @@ func main() {
 
 	text := tpl.Must(tpl.Execute(templateText))
 
+	if *edit {
+		editInPlace(text, templateText)
+		return
+	}
 	fmt.Print(text)
 }
